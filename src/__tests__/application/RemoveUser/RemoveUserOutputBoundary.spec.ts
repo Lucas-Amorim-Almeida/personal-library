68e1d32ec787582f4e8214ba7f33d029c8e1be35
Lucas-Amorim-Almeida <si.lucas.almeida@gmail.com>
2025-01-05 11:49:52 -0300

    test (use cases): add tests to get method of  RemoveUserOutputBoundary class.

--- src/__tests__/application/RemoveUser/RemoveUserOutputBoundary.spec.ts
+++ src/__tests__/application/RemoveUser/RemoveUserOutputBoundary.spec.ts
@@ -27,4 +27,54 @@ describe("RemoveUserOutputBoundary", () => {
       );
     });
   });
+
+  describe("get", () => {
+    it("Should return true", () => {
+      const params: dbOutputData = {
+        id: "id-0001",
+        status: "A DELETAR",
+        username: "john_doe",
+        password: "1234",
+        access_level: "COMMON",
+        created_at: new Date(2020, 2, 22),
+        updated_at: new Date(2022, 2, 22),
+      };
+
+      const output = new RemoveUserOutputBoundary(params);
+
+      expect(output.get()).toBe(true);
+    });
+
+    it("Should return false", () => {
+      const params: dbOutputData = {
+        id: "id-0001",
+        status: "BANIDO",
+        username: "john_doe",
+        password: "1234",
+        access_level: "COMMON",
+        created_at: new Date(2020, 2, 22),
+        updated_at: new Date(2022, 2, 22),
+      };
+
+      const output = new RemoveUserOutputBoundary(params);
+
+      expect(output.get()).toBe(false);
+    });
+
+    it("Should throws an error User status is not valid.", () => {
+      const params: dbOutputData = {
+        id: "id-0001",
+        status: "invalid",
+        username: "john_doe",
+        password: "1234",
+        access_level: "COMMON",
+        created_at: new Date(2020, 2, 22),
+        updated_at: new Date(2022, 2, 22),
+      };
+
+      const output = new RemoveUserOutputBoundary(params);
+
+      expect(() => output.get()).toThrow("User status is not valid.");
+    });
+  });
 });
